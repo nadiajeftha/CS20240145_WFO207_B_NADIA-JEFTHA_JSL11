8b54adff8cf4044a0b811db933ceec852413ea89
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-02 20:48:09 +0200

    fix bugs(addEventListener

--- index.js
+++ index.js
@@ -50,19 +50,19 @@ function fetchAndDisplayBoardsAndTasks() {
 function displayBoards(boards) {
   const boardsContainer = document.getElementById("boards-nav-links-div");
   boardsContainer.innerHTML = ''; // Clears the container
-  boards.forEach(board => {
+  boards.forEach((board) => {
     const boardElement = document.createElement("button");
     boardElement.textContent = board;
     boardElement.classList.add("board-btn");
-    boardElement.click()  { 
-      elements.headerBoardName.textContent = board;
+    boardElement.addEventListener("click" , () => {
+      elements.headerBoardName.textContent = board
       filterAndDisplayTasksByBoard(board);
       activeBoard = board //assigns active board
       localStorage.setItem("activeBoard", JSON.stringify(activeBoard))
       styleActiveBoard(activeBoard)
-    };
-    boardsContainer.appendChild(boardElement);
   });
+    boardsContainer.appendChild(boardElement);
+});
 
 }
 
