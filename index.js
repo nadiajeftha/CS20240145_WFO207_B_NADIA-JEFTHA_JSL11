b0ffa7c5d23c3ca55e272b45f56ec4f0835865c6
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-02 21:45:15 +0200

    fix bug

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function displayBoards(boards) {
 // TASK: Fix Bugs
 function filterAndDisplayTasksByBoard(boardName) {
   const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
+  const filteredTasks = tasks.filter(task => task.board === boardName);
 
   // Ensure the column titles are set outside of this function or correctly initialized before this function runs
 
@@ -92,7 +92,7 @@ function filterAndDisplayTasksByBoard(boardName) {
       taskElement.setAttribute('data-task-id', task.id);
 
       // Listen for a click event on each task and open a modal
-      taskElement.click() => { 
+      taskElement.addEventListener("click" , () => { 
         openEditTaskModal(task);
       });
 
